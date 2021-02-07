4df6c5264635d5a7881f921217ac6012d37808a4
Delhivery <delhivery@DV-LT-N-26186.local>
2021-02-07 10:10:31 +0530

    updating test case

--- src/App.test.js
+++ src/App.test.js
@@ -4,6 +4,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/Learn React2/i);
+  const linkElement = getByText(/Learn React/i);
   expect(linkElement).toBeInTheDocument();
 });
